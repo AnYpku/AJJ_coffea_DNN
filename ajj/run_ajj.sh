8b27f90d6c7c2e10b0ef70eab303d1dcbfa99ed9
Ying An <ying.an@cern.ch>
2022-12-02 13:48:12 +0100

    preliminary coffea test

--- ajj/run_ajj.sh
+++ ajj/run_ajj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date
 export HOME="/afs/cern.ch/user/y/yian/"
-python3 -u /afs/cern.ch/user/y/yian/work/DESY_pro/ajj/ajj.py --year $1 --samples $2 --outfile $3 --nproc $4
+python3 -u /afs/cern.ch/user/y/yian/work/DESY_pro/AJJ_coffea_DNN/ajj/ajj.py --year $1 --samples $2 --outfile $3 --nproc $4
 date
 cp $3 /eos/user/y/yian/AJJ_analysis/
 
